307d71d8ad5979d44a81996c245c7a89382c9260
final project <papiyalisa9@gmail.com>
2025-02-07 22:54:18 +0600

    commit six

--- app/src/main/java/com/example/finalproject/DatabaseHelper.java
+++ app/src/main/java/com/example/finalproject/DatabaseHelper.java
@@ -7,126 +7,82 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "finalproject.db";
-    public static final int DATABASE_VERSION = 2;
-    public static final String TABLE_TECHNOLOGIES = "recipe";
-    public static final String TABLE_USERS = "users";
 
-    // Common column
+    // Database Information
+    public static final String DATABASE_NAME = "recipe_master.db";
+    public static final int DATABASE_VERSION = 2;
+
+    // Table Names
+    public static final String TABLE_RECIPES = "recipes";
+
+    // Common Column
     public static final String COLUMN_ID = "id";
 
-    // Columns for recipe table
-    public static final String COLUMN_TECH_NAME = "recipe_name";
-    public static final String COLUMN_TECH_IMAGE = "recipe_image";
-
-    // Columns for users table
-    public static final String COLUMN_USERNAME = "username";
-    public static final String COLUMN_PASSWORD = "password";
-    public static final String COLUMN_PHONE = "phone";
-    public static final String COLUMN_RECIPE_IMAGE ="image" ;
+    // Columns for Recipes Table
+    public static final String COLUMN_RECIPE_NAME = "recipe_name";
+    public static final String COLUMN_RECIPE_IMAGE = "recipe_image";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TECHNOLOGIES);
-        onCreate(db);
+    public void onCreate(SQLiteDatabase db) {
+        // Create Recipes Table
+        String CREATE_RECIPES_TABLE = "CREATE TABLE " + TABLE_RECIPES + " ("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_RECIPE_NAME + " TEXT,"
+                + COLUMN_RECIPE_IMAGE + " BLOB)";
+        db.execSQL(CREATE_RECIPES_TABLE);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        // Create users table
-        String CREATE_USERS_TABLE = "CREATE TABLE " + TABLE_USERS + "("
-                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + COLUMN_USERNAME + " TEXT,"
-                + COLUMN_PASSWORD + " TEXT,"
-                + COLUMN_PHONE + " TEXT)";
-        db.execSQL(CREATE_USERS_TABLE);
-
-        // Create recipe table
-        String CREATE_TECHNOLOGIES_TABLE = "CREATE TABLE " + TABLE_TECHNOLOGIES + "("
-                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + COLUMN_TECH_NAME + " TEXT,"
-                + COLUMN_TECH_IMAGE + " BLOB)";
-        db.execSQL(CREATE_TECHNOLOGIES_TABLE);
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RECIPES);
+        onCreate(db);
     }
 
-    // Insert user into the database
-    public boolean insertUser(String username, String password, String phone) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_USERNAME, username);
-        values.put(COLUMN_PASSWORD, password);
-        values.put(COLUMN_PHONE, phone);
-
-        long result = db.insert(TABLE_USERS, null, values);
-        db.close();
-        return result != -1;
-    }
-
-    // Check if a user exists by phone number
-    public boolean checkUserExists(String phone) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_USERS, new String[]{COLUMN_ID}, COLUMN_PHONE + "=?", new String[]{phone}, null, null, null);
-        boolean exists = (cursor.getCount() > 0);
-        cursor.close();
-        db.close();
-        return exists;
-    }
-
-    // Check user by phone and password for login
-    public boolean checkUserByPhone(String userPhone, String userPassword) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_USERS + " WHERE " + COLUMN_PHONE + " = ? AND " + COLUMN_PASSWORD + " = ?", new String[]{userPhone, userPassword});
-        boolean exists = cursor.getCount() > 0;
-        cursor.close();
-        db.close();
-        return exists;
-    }
-
-    // Insert recipe into the database
+    // Insert Recipe into Database
     public boolean insertRecipe(String recipeName, byte[] recipeImage) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COLUMN_TECH_NAME, recipeName);
-        values.put(COLUMN_TECH_IMAGE, recipeImage);
+        values.put(COLUMN_RECIPE_NAME, recipeName);
+        values.put(COLUMN_RECIPE_IMAGE, recipeImage);
 
-        long result = db.insert(TABLE_TECHNOLOGIES, null, values);
+        long result = db.insert(TABLE_RECIPES, null, values); // Returns a row ID or -1 if failed
         db.close();
+
         return result != -1;
     }
 
-    // Get all recipes
+    // Get All Recipes
     public Cursor getAllRecipes() {
         SQLiteDatabase db = this.getReadableDatabase();
-        String[] projection = {COLUMN_ID + " AS _id", COLUMN_TECH_NAME, COLUMN_TECH_IMAGE};
-        return db.query(TABLE_TECHNOLOGIES, projection, null, null, null, null, null);
+        String[] projection = {COLUMN_ID + " AS _id", COLUMN_RECIPE_NAME, COLUMN_RECIPE_IMAGE};
+        return db.query(TABLE_RECIPES, projection, null, null, null, null, null);
     }
 
-    // Get a specific recipe by name
+    // Get Recipe by Name
     public Cursor getRecipeByName(String recipeName) {
         SQLiteDatabase db = this.getReadableDatabase();
-        return db.rawQuery("SELECT * FROM " + TABLE_TECHNOLOGIES + " WHERE " + COLUMN_TECH_NAME + " = ?", new String[]{recipeName});
+        return db.query(TABLE_RECIPES, null, COLUMN_RECIPE_NAME + "=?", new String[]{recipeName}, null, null, null);
     }
 
-    // Update a recipe
-    public void updateRecipe(int recipeId, String recipeName, byte[] recipeImage) {
+    // Update Recipe Image by Name
+    public boolean updateRecipe(String recipeName, byte[] image) {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_TECH_NAME, recipeName);
-        values.put(COLUMN_TECH_IMAGE, recipeImage);
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_RECIPE_IMAGE, image);
 
-        db.update(TABLE_TECHNOLOGIES, values, COLUMN_ID + " = ?", new String[]{String.valueOf(recipeId)});
-        db.close();
+        int rowsUpdated = db.update(TABLE_RECIPES, contentValues, COLUMN_RECIPE_NAME + "=?", new String[]{recipeName});
+
+        return rowsUpdated > 0;
     }
 
-    // Delete a recipe by name
-    public void deleteRecipe(String recipeName) {
+    // Delete Recipe by Name
+    public boolean deleteRecipe(String recipeName) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_TECHNOLOGIES, COLUMN_TECH_NAME + " = ?", new String[]{recipeName});
-        db.close();
+        int rowsDeleted = db.delete(TABLE_RECIPES, COLUMN_RECIPE_NAME + "=?", new String[]{recipeName});
+        return rowsDeleted > 0;
     }
 }
--- app/src/main/java/com/example/finalproject/UpdateRecipeActivity.java
+++ app/src/main/java/com/example/finalproject/UpdateRecipeActivity.java
@@ -1,16 +1,15 @@
 package com.example.finalproject;
+
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -24,61 +23,63 @@ public class UpdateRecipeActivity extends AppCompatActivity {
     private static final int PICK_IMAGE_REQUEST = 1;
 
     private EditText editTextName;
-    private ImageView imageViewrecipe;
-    private Button buttonUpdate;
+    private ImageView imageViewRecipe;
     private Button buttonSelectImage;
     private Button buttonSearch;
-
+    private Button buttonUpdate;
 
     private DatabaseHelper databaseHelper;
     private byte[] recipeImageByteArray;
-    private View imageviewrecipe;
-    private String recipe_name;
-    private byte[] image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_recipe);
 
+        // Initialize the views
         editTextName = findViewById(R.id.edit_text_recipe_name);
-        imageviewrecipe = findViewById(R.id.image_view_recipe);
-        buttonUpdate = findViewById(R.id.button_update);
+        imageViewRecipe = findViewById(R.id.image_view_recipe);
         buttonSelectImage = findViewById(R.id.button_select_image);
         buttonSearch = findViewById(R.id.button_search);
-
+        buttonUpdate = findViewById(R.id.button_update);
 
         databaseHelper = new DatabaseHelper(this);
 
-        buttonSearch.setOnClickListener(view -> searchProduct(image));
+        // Set button actions
+        buttonSearch.setOnClickListener(view -> searchRecipe());
         buttonSelectImage.setOnClickListener(view -> selectImage());
-        buttonUpdate.setOnClickListener(view -> updateProduct());
+        buttonUpdate.setOnClickListener(view -> updateRecipe());
     }
 
-    private void searchProduct(byte[] image) {
+    // Search for a recipe by name
+    private void searchRecipe() {
         String recipeName = editTextName.getText().toString().trim();
+
         if (recipeName.isEmpty()) {
             Toast.makeText(this, "Please enter a recipe name to search", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        Cursor cursor = databaseHelper.getRecipeByName(recipe_name);
+        Cursor cursor = databaseHelper.getRecipeByName(recipeName);
 
-        if (cursor != null && cursor.moveToFirst())
-        {
-            if (null != image) {
+        if (cursor != null && cursor.moveToFirst()) {
+            // Fetch recipe data
+            byte[] image = cursor.getBlob(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_RECIPE_IMAGE));
+
+            // Set the image to the ImageView
+            if (image != null) {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
-                imageViewrecipe.setImageBitmap(bitmap);
+                imageViewRecipe.setImageBitmap(bitmap);
                 recipeImageByteArray = image;
             }
+
             cursor.close();
-        }
-        else
-        {
+        } else {
             Toast.makeText(this, "Recipe not found", Toast.LENGTH_SHORT).show();
         }
     }
 
+    // Select an image for the recipe
     private void selectImage() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
@@ -88,32 +89,37 @@ public class UpdateRecipeActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri imageUri = data.getData();
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-                imageViewrecipe.setImageBitmap(bitmap);
+                // Get the image URI and convert it to bitmap
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                imageViewRecipe.setImageBitmap(bitmap);
+
+                // Convert bitmap to byte array
                 ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
                 recipeImageByteArray = byteArrayOutputStream.toByteArray();
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void updateProduct() {
+    // Update the recipe details
+    private void updateRecipe() {
+        String recipeName = editTextName.getText().toString().trim();
 
-        String tecName = editTextName.getText().toString().trim();
-
-
-        if (tecName.isEmpty())
-        {
-            Toast.makeText(this, "Please give recipe name", Toast.LENGTH_SHORT).show();
+        if (recipeName.isEmpty()) {
+            Toast.makeText(this, "Please enter a recipe name", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        // Call the method to update the recipe in the database
+        boolean isUpdated = databaseHelper.updateRecipe(recipeName, recipeImageByteArray);
 
-
+        if (isUpdated) {
+            Toast.makeText(this, "Recipe updated successfully", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Failed to update recipe", Toast.LENGTH_SHORT).show();
+        }
     }
 }
